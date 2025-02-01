e8ce2e64b6980839b27baf56b07cd311cab7c38a
TurkishCodeMan <huseyinaltikulac4@gmail.com>
2025-02-01 22:02:02 +0300

    fixeed,,

--- drill/next.config.js
+++ drill/next.config.js
@@ -3,13 +3,7 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': __dirname,
-      '@/components': __dirname + '/components',
-      '@/features': __dirname + '/features',
-      '@/lib': __dirname + '/lib',
-      '@/styles': __dirname + '/styles',
-      '@/types': __dirname + '/types',
-      '@/utils': __dirname + '/utils'
+      '@': __dirname
     };
     return config;
   }

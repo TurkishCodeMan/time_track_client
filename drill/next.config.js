875ad3b397f8176696b65208d6e13b223f5f2155
TurkishCodeMan <huseyinaltikulac4@gmail.com>
2025-02-02 19:32:40 +0300

    tt

--- drill/next.config.js
+++ drill/next.config.js
@@ -10,13 +10,25 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    outputFileTracingRoot: process.env.VERCEL ? undefined : process.cwd(),
-  },
-  distDir: '.next',
-  poweredByHeader: false,
+  assetPrefix: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : '',
+  basePath: '',
+  trailingSlash: false,
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
+  }
 }
 
 module.exports = nextConfig 

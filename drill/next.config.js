075b882585a162b3b3327d092063e56acc21e459
TurkishCodeMan <huseyinaltikulac4@gmail.com>
2025-02-02 19:29:32 +0300

    ff

--- drill/next.config.js
+++ drill/next.config.js
@@ -12,18 +12,11 @@ const nextConfig = {
   },
   experimental: {
     outputFileTracingRoot: process.env.VERCEL ? undefined : process.cwd(),
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        'node_modules/@esbuild/linux-x64',
-      ],
-    },
   },
-  distDir: process.env.VERCEL ? '.vercel/output/static' : '.next',
-  generateBuildId: async () => {
-    return 'build-' + Date.now()
-  }
+  distDir: '.next',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true
 }
 
 module.exports = nextConfig 

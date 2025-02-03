c0656c648567d2ad5360dd3a15da082fcb106cf2
TurkishCodeMan <huseyinaltikulac4@gmail.com>
2025-02-03 19:11:50 +0300

    ffff

--- drill/lib/api.ts
+++ drill/lib/api.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  baseURL: 'https://wersiyon44.pythonanywhere.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
